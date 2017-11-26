c6e2b577a4704d27c323e4fefbb7f645f91c5864
datnguyen18 <30653120+datnguyen18@users.noreply.github.com>
2017-11-26 09:57:13 +0700

    a

--- src/Screens/SettingConversation.js
+++ src/Screens/SettingConversation.js
@@ -14,7 +14,7 @@ window.Blob = polyfill.Blob;
 // More info on all the options is below in the README...just some common use cases shown here
 var options = {
     title: "Chọn ảnh từ:",
-    quality: 0.6,
+    quality: 0.3,
     storageOptions: {
         skipBackup: true,
         path: 'images'
